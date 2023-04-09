a4146a9a333aa2cbeefee6eda689ee0af2f595bf
eXtream5890 <114796785+eXtream5890@users.noreply.github.com>
2023-04-09 15:25:24 +0530

    Update lunar_eclipse_data.js

--- lunar_eclipse_data.js
+++ lunar_eclipse_data.js
@@ -1,7 +1,103 @@
 const lunar_eclipse_data = [
     {
         id : 1,
-        date : 25-03-2022,
-        lunar: uifhdsiuf
+        date : 08-04-2024,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_April_8,_2024
     }
+     {
+        id : 2,
+        date : 02-10-2024,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_October_2,_2024
+    }
+     {
+        id : 3,
+        date : 12-08-2026,
+       solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_August_12,_2026
+    }
+ {
+        id : 4,
+        date : 25-03-2027,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_February_6,_2027
+    }
+ {
+        id : 5,
+        date : 26-01-2028,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_January_26,_2028
+    }
+ {
+        id : 6,
+        date : 25-1-2030,
+       solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_November_25,_2030
+    }
+ {
+        id : 7,
+        date : 31-05-2031,
+        solar: https://en.wikipedia.org/wiki/Annular_solar_eclipse
+    }
+      {
+        id : 8,
+        date : 14-11-2031,
+        solar: https://en.wikipedia.org/wiki/Hybrid_solar_eclipse
+    }
+      {
+        id : 9,
+        date :  20-03-2034,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_March_20,_2034
+    }
+      {
+        id : 10,
+        date : 12-09-2034,
+        solar: https://en.wikipedia.org/wiki/Annular_solar_eclipse
+    }
+      {
+        id : 11,
+        date : 05-09-2038,
+        solar:https://en.wikipedia.org/wiki/Solar_eclipse_of_January_5,_2038
+    }
+      {
+        id : 12,
+        date : 02-07-2038,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_July_2,_2038
+    }
+     {
+        id : 13,
+        date : 2038 December 26,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_December_26,_2038
+    }
+     {
+        id : 14,
+        date : 2039 June 21,
+        solar:https://en.wikipedia.org/wiki/Solar_eclipse_of_June_21,_2039
+    }
+     {
+        id : 15,
+        date :2039 December 15,
+        solar:https://en.wikipedia.org/wiki/Total_solar_eclipse
+    }
+     {
+        id : 16,
+        date : 02-07-2038,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_July_2,_2038
+    }
+     {
+        id : 17,
+        date : 02-07-2038,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_July_2,_2038
+    }
+     {
+        id : 18,
+        date : 02-07-2038,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_July_2,_2038
+    }
+     {
+        id : 19,
+        date : 02-07-2038,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_July_2,_2038
+    }
+     {
+        id : 20,
+        date : 02-07-2038,
+        solar: https://en.wikipedia.org/wiki/Solar_eclipse_of_July_2,_2038
+    }
+    
 ]
